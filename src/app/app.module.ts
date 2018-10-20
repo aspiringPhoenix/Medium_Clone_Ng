bbd2a6f46b43d2fcbbfcb01706aa68de28421441
kumarkunal <kkunal221b@gmail.com>
2018-10-20 14:32:43 +0530

    Basic routing to homepage , header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes,RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'home',component:HomeComponent},
+      {path:'',redirectTo:'home',pathMatch:'full'},
+      {path:'**',redirectTo:'home',pathMatch:'full'}
+
+    ])
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
