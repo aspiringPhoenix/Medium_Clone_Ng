626d80019294818858d4a8d0f7282d85625ba829
kumarkunal <kkunal221b@gmail.com>
2018-10-21 21:34:22 +0530

    Basic Sign up and Login Authorization with corresponding model and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {Routes,RouterModule} from '@angular/router';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-
+import { AuthComponent } from './auth/auth/auth.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,12 +16,19 @@ import { FooterComponent } from './shared/footer/footer.component';
     HomeComponent,
     HeaderComponent,
     FooterComponent,
+    AuthComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    SharedModule,
     RouterModule.forRoot([
       {path:'home',component:HomeComponent},
       {path:'',redirectTo:'home',pathMatch:'full'},
+      {path: 'login',component: AuthComponent},
+      {path: 'register', component: AuthComponent},
       {path:'**',redirectTo:'home',pathMatch:'full'}
 
     ])
